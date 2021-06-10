2dd5daeb11ee98e32a377a38ad4bb2aaaa7115cf
Jesus Castro <jjoesei.castro@gmail.com>
2021-06-11 01:43:02 +0800

    Add BN43a jjesuscastro layout

---
+++ keyboards/bm43a/keymaps/jjesuscastro/rules.mk
@@ -0,0 +1,5 @@
+BOOTMAGIC_ENABLE = lite       # Virtual DIP switch configuration
+COMMAND_ENABLE = no         # Commands for debug and configuration
+CONSOLE_ENABLE = no         # Console for debug
+NKRO_ENABLE = yes            # USB Nkey Rollover
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
